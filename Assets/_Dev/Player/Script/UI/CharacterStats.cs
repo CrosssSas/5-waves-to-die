cf8afab2549f2b5024418dff568928ed4ca6b5bc
ZZZaky <yurakalinin29@gmail.com>
2022-07-03 15:07:24 +0300

    adding healthbar

---
+++ Assets/_Dev/Player/Script/UI/CharacterStats.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [SerializeField] public int currentHealth;
+    [SerializeField] public int maxHealth;
+    [SerializeField] public bool isDead;
+
+
+    private void Start()
+    {
+        InitVariables();
+    }
+
+    public virtual void CheckHealth()
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+        if (currentHealth >= maxHealth) { currentHealth = maxHealth; }
+    }
+
+    public void SetHealthTo(int amount)
+    {
+        currentHealth = amount;
+        CheckHealth();
+    }
+
+    public void TakeDamage(int amount) { SetHealthTo(currentHealth - amount); }
+
+    public void Heal(int amount) { SetHealthTo(currentHealth + amount); }
+
+    public virtual void InitVariables()
+    {
+        maxHealth = 100;
+        SetHealthTo(maxHealth);
+        isDead = false;
+    }
+
+    public virtual void Die() 
+    { 
+        isDead = true; 
+        // DIE mechanic
+    }
+}
